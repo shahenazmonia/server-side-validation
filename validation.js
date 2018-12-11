6eb3de02dc50fbad99d25777ef6927d0c7715fe5
shahenazmonia <shahy.m.93@gmail.com>
2018-12-11 16:21:32 +0200

    delete unrequired data

--- validation.js
+++ validation.js
@@ -2,15 +2,8 @@ const Joi = require('joi');
 
 const signupValidation = {
 body: {
-  username: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required().regex(/^[a-zA-Z0-9]{3,30}$/),
-  confirmPassword: Joi.string().required()
-  .valid(Joi.ref('password')
-  // .option({language:{any:{allowOnly:'Password not match'}}})
-),
-  role: Joi.string().valid(['admin','user']).required(),
-  mobile: Joi.number(),
+  // write validation here 
+
   }
 };
 
