df5eb88636573cdd724f88f6d853274f9720f09a
shahenazmonia <shahy.m.93@gmail.com>
2018-12-11 15:02:27 +0200

    create html,css, and fetch file to handle a front-end response

---
+++ public/fetch.js
@@ -0,0 +1,38 @@
+
+const button = document.getElementById('submit');
+
+
+const signup = function(url = ``, data = {}) {
+     return fetch(url, {
+        method: "POST",
+        credentials: 'include',
+         redirect: "follow",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify(data),
+    })
+    .then(function (response){
+    return  response.json();
+    }).then(res=>{
+      console.log('response',res);
+    })
+}
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const username= document.getElementById('username').value;
+  const email= document.getElementById('email').value;
+  const password= document.getElementById('password').value;
+  const confirmPassword= document.getElementById('confirmPassword').value;
+  const role= document.getElementById('role').value;
+  const mobile= document.getElementById('mobile').value;
+signup('/signup', {
+  username,
+  email,
+  password,
+  confirmPassword,
+  role,
+  mobile
+})
+})
